5fc9723b81a9cdddc4995597710017df99dd2075
BurnSoft <bs.github@burnsoft.net>
2026-02-18 11:08:02 -0500

    #3 Added unit tests for autofill function

---
+++ BurnSoft.Applications.MLL.UnitTests/AutoFill/GeneralShotgunTest.cs
@@ -0,0 +1,80 @@
+﻿using BurnSoft.Applications.MLL.AutoFill;
+using BurnSoft.Applications.MLL.UnitTests.Settings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+
+namespace BurnSoft.Applications.MLL.UnitTests.AutoFill
+{
+    [TestClass]
+    public class GeneralShotgunTest
+    {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The error out
+        /// </summary>
+        private string _errOut;
+        /// <summary>
+        /// The database path
+        /// </summary>
+        private string _databasePath;
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            // Vs2019.GetSetting("");
+            _errOut = @"";
+            _databasePath = Vs2019.GetSetting("DatabasePath");
+        }
+
+        [TestMethod, TestCategory("AutoFill - Shotgun General")]
+        public void CaseManufacturerTest()
+        {
+            AutoCompleteStringCollection value = GeneralShotgun.CaseManufacturer(_databasePath, out _errOut);
+            foreach (var a in value)
+            {
+                TestContext.WriteLine(a.ToString());
+            }
+            General.HasTrueValue(value.Count > 0, _errOut);
+        }
+
+        [TestMethod, TestCategory("AutoFill - Shotgun General")]
+        public void CaseNameTest()
+        {
+            AutoCompleteStringCollection value = GeneralShotgun.CaseName(_databasePath, out _errOut);
+            foreach (var a in value)
+            {
+                TestContext.WriteLine(a.ToString());
+            }
+            General.HasTrueValue(value.Count > 0, _errOut);
+        }
+
+        [TestMethod, TestCategory("AutoFill - Shotgun General")]
+        public void DramTest()
+        {
+            AutoCompleteStringCollection value = GeneralShotgun.Dram(_databasePath, out _errOut);
+            foreach (var a in value)
+            {
+                TestContext.WriteLine(a.ToString());
+            }
+            General.HasTrueValue(value.Count > 0, _errOut);
+        }
+
+        [TestMethod, TestCategory("AutoFill - Shotgun General")]
+        public void GaugeTest()
+        {
+            AutoCompleteStringCollection value = GeneralShotgun.Gauge(_databasePath, out _errOut);
+            foreach (var a in value)
+            {
+                TestContext.WriteLine(a.ToString());
+            }
+            General.HasTrueValue(value.Count > 0, _errOut);
+        }
+    }
+}
