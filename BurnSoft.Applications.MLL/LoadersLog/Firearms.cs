19e935993621f82828b891e43bf4b71a6b73f5c3
BurnSoft <bs.github@burnsoft.net>
2026-02-19 10:37:06 -0500

    #1   Added more Base functions for the firearm collection class

--- BurnSoft.Applications.MLL/LoadersLog/Firearms.cs
+++ BurnSoft.Applications.MLL/LoadersLog/Firearms.cs
@@ -1,5 +1,6 @@
 ﻿using BurnSoft.Applications.MLL.Types;
 using BurnSoft.Security.RegularEncryption.SHA;
+using BurnSoft.Universal;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -67,8 +68,15 @@ namespace BurnSoft.Applications.MLL.LoadersLog
         private static string ErrorMessage(string functionName, ArgumentNullException e) =>
             $"{ClassLocation}.{functionName} - {e.Message}";
 
-        #endregion        
+        #endregion
 
+        /// <summary>
+        /// Gets the data from the data table and puts it in for the 
+        /// FirearmCollection list container
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;FirearmCollection&gt;.</returns>
         private static List<FirearmCollection> GetData(DataTable dt, out string errOut)
         {
             List<FirearmCollection> lst = new List<FirearmCollection>();
@@ -98,6 +106,210 @@ namespace BurnSoft.Applications.MLL.LoadersLog
                 errOut = ErrorMessage("GetData", e);
             }
             return lst;
+        }
+        /// <summary>
+        /// Gets the details of the selected firearm
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;FirearmCollection&gt;.</returns>
+        /// <exception cref="System.Exception"></exception>
+        public static List<FirearmCollection> GetDetails(string databasePath, int id, out string errOut)
+        {
+            string sql = $"Select * from Loaders_Log_Firearms where ID={id}";
+            return GetList(databasePath, sql, out errOut);
+        }
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;FirearmCollection&gt;.</returns>
+        public static List<FirearmCollection> GetAll(string databasePath, out string errOut)
+        {
+            string sql = $"Select * from Loaders_Log_Firearms order by FullName ASC";
+            return GetList(databasePath, sql, out errOut);
+        }
+        /// <summary>
+        /// Gets the list.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>List&lt;FirearmCollection&gt;.</returns>
+        /// <exception cref="System.Exception"></exception>
+        private static List<FirearmCollection> GetList(string databasePath, string sql, out string errOut)
+        {
+            List<FirearmCollection> lst = new List<FirearmCollection>();
+            errOut = "";
+            try
+            {
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                lst = GetData(dt, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetList", e);
+            }
+            return lst;
+        }
+
+        /// <summary>
+        /// Gets the firearm identifier.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="fullName">The full name.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.Int64.</returns>
+        /// <exception cref="System.Exception"></exception>
+        public static long GetId(string databasePath, string fullName, out string errOut)
+        {
+            errOut = "";
+            long lAns = 0;
+            try
+            {
+                string sql = $"Select * from Loaders_Log_Firearms where fullname='{fullName}'";
+                List<FirearmCollection> lst = GetList(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                foreach (FirearmCollection i in lst)
+                {
+                    lAns = i.Id;
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetFirearmId", e);
+            }
+            return lAns;
+        }
+
+        /// <summary>
+        /// Adds the firearm to the loaders log table to use for sample logging testing.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="manufacturer">The manufacturer.</param>
+        /// <param name="model">The model.</param>
+        /// <param name="serial">The serial.</param>
+        /// <param name="caliber">The caliber.</param>
+        /// <param name="type">The type.</param>
+        /// <param name="barrel">The barrel.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <param name="mgcId">The MGC identifier.</param>
+        /// <param name="exclude">if set to <c>true</c> [exclude].</param>
+        /// <param name="fullName">The full name.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool Add(string databasePath, string manufacturer, string model, string serial, 
+            string caliber, string type, string barrel,  out string errOut, int mgcId = 0, 
+            bool exclude = false, string fullName = "")
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                BSOtherObjects o = new BSOtherObjects();
+                fullName = fullName.Length == 0 ? $"{manufacturer} {model}" : fullName;
+                int iExclude = exclude ? 0 : 1;
+                string sql = $"INSERT INTO Loaders_Log_Firearms (MGCID,FullName,Manu,Model,Cal,Barrel,SerialNo," +
+                    $"GType,exclude) VALUES({mgcId},'{o.FC(fullName)}', '{o.FC(manufacturer)}', '{o.FC(model)}', " +
+                    $"'{o.FC(caliber)}', '{o.FC(barrel)}', '{o.FC(serial)}', '{o.FC(type)}', {iExclude})";
+
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Add", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Updates the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="manufacturer">The manufacturer.</param>
+        /// <param name="model">The model.</param>
+        /// <param name="serial">The serial.</param>
+        /// <param name="caliber">The caliber.</param>
+        /// <param name="type">The type.</param>
+        /// <param name="barrel">The barrel.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <param name="mgcId">The MGC identifier.</param>
+        /// <param name="exclude">if set to <c>true</c> [exclude].</param>
+        /// <param name="fullName">The full name.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool Update(string databasePath, int id, string manufacturer, string model, string serial,
+            string caliber, string type, string barrel, out string errOut, int mgcId = 0,
+            bool exclude = false, string fullName = "")
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                BSOtherObjects o = new BSOtherObjects();
+                fullName = fullName.Length == 0 ? $"{manufacturer} {model}" : fullName;
+                int iExclude = exclude ? 0 : 1;
+                string sql = $"UPDATE Loaders_Log_Firearms set MGCID={mgcId}," +
+                    $"FullName='{o.FC(fullName)}',Manu='{o.FC(manufacturer)}'," +
+                    $"Model='{o.FC(model)}',Cal='{o.FC(caliber)}',Barrel='{o.FC(barrel)}'," +
+                    $"SerialNo='{o.FC(serial)}',GType='{o.FC(type)}',exclude={iExclude} where id={id}";
+
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Update", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Deletes the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool Delete(string databasePath, long id, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                string sql = $"DELETE from Loaders_Log_Firearms where id={id}";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+            return bAns;
+        }
+        /// <summary>
+        /// Deletes the specified database path.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="fullName">The full name.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="System.Exception"></exception>
+        public static bool Delete(string databasePath, string fullName, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                long id = GetId(databasePath, fullName, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                bAns = Delete(databasePath, id, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+            return bAns;
         }
     }
 }
