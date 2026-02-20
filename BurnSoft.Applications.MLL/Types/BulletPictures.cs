98ebaf00c334bd969ce6878c95ad97b883b13fa1
BurnSoft <bs.github@burnsoft.net>
2026-02-20 09:12:11 -0500

    @1 Adding bullet inventory tables

---
+++ BurnSoft.Applications.MLL/Types/BulletPictures.cs
@@ -0,0 +1,40 @@
+﻿
+namespace BurnSoft.Applications.MLL.Types
+{
+    //TODO: Determine if this is still a feature that you wish to have for the application
+
+    /// <summary>
+    /// Class BulletPictures list container for the List_Bullets_Picture Table.
+    /// This was put in some time ago but never used in the application.  Might
+    /// Come back to this and add it, still Need to Create the functions to 
+    /// interact with the table and data.
+    /// </summary>
+    public class BulletPictures
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the bullet identifier. Column name is BID
+        /// </summary>
+        /// <value>The bullet identifier.</value>
+        public int BulletId { get; set; }
+        /// <summary>
+        /// Gets or sets the picute BLOB. Column Name is Pic_Blob
+        /// </summary>
+        /// <value>The picute BLOB.</value>
+        public object PicuteBlob { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is default.
+        /// </summary>
+        /// <value><c>true</c> if this instance is default; otherwise, <c>false</c>.</value>
+        public bool IsDefault { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
---
+++ BurnSoft.Applications.MLL/Types/BulletsSingleUse.cs
@@ -0,0 +1,35 @@
+﻿
+namespace BurnSoft.Applications.MLL.Types
+{
+    //TODO: Determine if this is still a feature that you wish to have for the application
+
+    /// <summary>
+    /// Class BulletsSingleUse list container for the List_Bullets_SU table.
+    /// This table was created to help with bullets that applied to more than one
+    /// caliber.  Something like .355 9mm 115 can apply to .380.  Some other diameter 
+    /// bullets can apply to other calibers
+    /// </summary>
+    public class BulletsSingleUse
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the suggested used identifier.  Column Name is SUID
+        /// </summary>
+        /// <value>The suggested used identifier.</value>
+        public int SuggestedUsedId { get; set; }
+        /// <summary>
+        /// Gets or sets the bullet identifier. Column Name is BulletID
+        /// </summary>
+        /// <value>The bullet identifier.</value>
+        public int BulletId { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
