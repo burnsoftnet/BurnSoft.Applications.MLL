735136bec6d496f717545a7bb044f481904d087d
BurnSoft <bs.github@burnsoft.net>
2026-02-23 10:41:50 -0500

    #1  added list container for the listing sg bushings table

---
+++ BurnSoft.Applications.MLL/Types/ShotgunBushingListings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MLL.Types
+{
+    /// <summary>
+    /// Class ShotgunBushing is the list container for the List_SG_Bushing Table
+    /// </summary>
+    public class ShotgunBushingListings
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer.
+        /// </summary>
+        /// <value>The manufacturer.</value>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the charge for the sCharge Column
+        /// </summary>
+        /// <value>The charge.</value>
+        public string Charge { get; set; }
+        /// <summary>
+        /// Gets or sets for shot ID
+        /// </summary>
+        /// <value>For shot.</value>
+        public long ForShot { get; set; }
+        /// <summary>
+        /// Gets or sets for powder Id
+        /// </summary>
+        /// <value>For powder.</value>
+        public long ForPowder { get; set; }
+    }
+}
