562729a9ad64c0931cb53ab0221df61c3380f56e
BurnSoft <bs.github@burnsoft.net>
2026-02-25 15:54:37 -0500

    #1 added list container for the equipment

---
+++ BurnSoft.Applications.MLL/Types/EquipmentLists.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BurnSoft.Applications.MLL.Types
+{
+    /// <summary>
+    /// Class EquipmentLists list container for the General_Equipment table
+    /// </summary>
+    public class EquipmentLists
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer.
+        /// </summary>
+        /// <value>The manufacturer.</value>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the diameter.
+        /// </summary>
+        /// <value>The diameter.</value>
+        public string Use { get; set; }
+        /// <summary>
+        /// Gets or sets the cost.
+        /// </summary>
+        /// <value>The cost.</value>
+        public double Cost { get; set; }
+        /// <summary>
+        /// Gets or sets the notes.
+        /// </summary>
+        /// <value>The notes.</value>
+        public string Notes { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
