879a99590c38620f65dd497fdc68bf189b461b61
BurnSoft <bs.github@burnsoft.net>
2026-03-04 07:43:58 -0500

    #1 Added Primer Listings container

---
+++ BurnSoft.Applications.MLL/Types/PrimerListings.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace BurnSoft.Applications.MLL.Types
+{
+    public class PrimerListings
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer.
+        /// </summary>
+        /// <value>The manufacturer.</value>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the primer type identifier.
+        /// </summary>
+        /// <value>The primer type identifier.</value>
+        public int PrimerTypeId { get; set; }
+        /// <summary>
+        /// Gets or sets the type of the primer.
+        /// </summary>
+        /// <value>The type of the primer.</value>
+        public string PrimerType { get; set; }
+        /// <summary>
+        /// Gets or sets the qty.
+        /// </summary>
+        /// <value>The qty.</value>
+        public int Qty { get; set; }
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>The price.</value>
+        public double Price { get; set; }
+        /// <summary>
+        /// Gets or sets the price per primer.
+        /// </summary>
+        /// <value>The price per primer.</value>
+        public double PricePerPrimer { get; set; }
+        // <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
