ad11ffa3716f99b5a029cc7d18ef4f137e36d6b7
BurnSoft <bs.github@burnsoft.net>
2026-03-18 11:59:30 -0400

    #1 added list container for the Search_Fields table

---
+++ BurnSoft.Applications.MLL/Types/SearchFieldsData.cs
@@ -0,0 +1,35 @@
+﻿
+namespace BurnSoft.Applications.MLL.Types
+{
+    /// <summary>
+    /// Class SearchFieldsData list container for the Search_Fields table
+    /// </summary>
+    public class SearchFieldsData
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the description. For the Dis Column
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the column. for the colname column
+        /// </summary>
+        /// <value>The name of the column.</value>
+        public string ColumnName { get; set; }
+        /// <summary>
+        /// Gets or sets the type of the column. for the cType column
+        /// </summary>
+        /// <value>The type of the column.</value>
+        public string ColumnType { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
