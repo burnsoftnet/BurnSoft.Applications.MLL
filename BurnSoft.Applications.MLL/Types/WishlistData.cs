53a49b1b605e8c71d5f1a942a525e866b4223cdb
BurnSoft <bs.github@burnsoft.net>
2026-03-18 16:06:34 -0400

    #1 added wishlist list container and data handler

---
+++ BurnSoft.Applications.MLL/Types/WishlistData.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BurnSoft.Applications.MLL.Types
+{
+    /// <summary>
+    /// Class WishlistData list container for the wishlist table
+    /// </summary>
+    public class WishlistData
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public long Id {  get; set; }
+        /// <summary>
+        /// Gets or sets the manufacturer.
+        /// </summary>
+        /// <value>The manufacturer.</value>
+        public string Manufacturer { get; set; }
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        /// <value>The model.</value>
+        public string Model { get; set; }
+        /// <summary>
+        /// Gets or sets the plase to buy.
+        /// </summary>
+        /// <value>The plase to buy.</value>
+        public string PlaceToBuy { get; set; }
+        /// <summary>
+        /// Gets or sets the qty.
+        /// </summary>
+        /// <value>The qty.</value>
+        public string Qty { get; set; }
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Value { get; set; }
+        /// <summary>
+        /// Gets or sets the notes.
+        /// </summary>
+        /// <value>The notes.</value>
+        public string Notes { get; set; }
+    }
+}
