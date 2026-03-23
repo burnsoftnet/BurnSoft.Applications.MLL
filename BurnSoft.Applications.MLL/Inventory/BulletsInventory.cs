aaeffb28c2eaa1ed6155489dcf194285d08d4035
BurnSoft <bs.github@burnsoft.net>
2026-03-23 10:21:52 -0400

    #19 Added the Update Qty Function to bullets

--- BurnSoft.Applications.MLL/Inventory/BulletsInventory.cs
+++ BurnSoft.Applications.MLL/Inventory/BulletsInventory.cs
@@ -1,4 +1,5 @@
-﻿using BurnSoft.Applications.MLL.Types;
+﻿using BurnSoft.Applications.MLL.Helpers;
+using BurnSoft.Applications.MLL.Types;
 using BurnSoft.Universal;
 using System;
 using System.Collections.Generic;
@@ -358,6 +359,39 @@ namespace BurnSoft.Applications.MLL.Inventory
             }
             return bAns;
         }
+
+        public static bool UpdateQty(string databasePath, long id, int currentQty, double currentPrice,
+            double currentPricePerItem, int newQty, double NewPrice, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                int qty = currentQty + newQty;
+                double price = (currentQty * currentPricePerItem) + NewPrice;
+                double estCostPerItem = Converters.ConvertToDollars((price == 0) ? 0 : (price / qty));
+                string sql = $"UPDATE List_Bullets set Qty={qty}," +
+                    $"Price={price}, eppb={estCostPerItem} where id={id}";
+
+                if (currentPricePerItem == estCostPerItem)
+                {
+                    sql = $"UPDATE List_Bullets set Qty={newQty}," +
+                    $"Price={NewPrice} where id={id}";
+                }
+                else if (NewPrice == 0 && newQty == 0)
+                {
+                    sql = $"UPDATE List_Bullets set Qty=0, Price=0, eppb=0 where id={id}";
+                }
+
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("UpdateQty", e);
+            }
+            return bAns;
+        }
+
         /// <summary>
         /// Deletes the specified database path.
         /// </summary>
