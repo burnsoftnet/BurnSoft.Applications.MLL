08e3a6d7cc9d3e88d1d12a0394652d697579d65a
BurnSoft <bs.github@burnsoft.net>
2026-03-25 08:06:08 -0400

    #19 added missing function TotalConfigByCal

--- BurnSoft.Applications.MLL/Inventory/CaliberInventory.cs
+++ BurnSoft.Applications.MLL/Inventory/CaliberInventory.cs
@@ -3,6 +3,7 @@ using BurnSoft.Universal;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Net.NetworkInformation;
 
 namespace BurnSoft.Applications.MLL.Inventory
 {
@@ -187,6 +188,20 @@ namespace BurnSoft.Applications.MLL.Inventory
             }
             return sAns;
         }
+
+        /// <summary>
+        /// Get the Total count of the configuration used by caliber.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The caliber identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.Int64.</returns>
+        public static long TotalConfigurationUsedByCaliber(string databasePath, long id, out string errOut)
+        {
+            string sql = $"SELECT Count(*) as Total from qry_CFG_SR_PowderList where MyCalID={id}";
+            return Database.GetCount(databasePath, sql, out errOut);
+        }
+
         /// <summary>
         /// Gets the details.
         /// </summary>
