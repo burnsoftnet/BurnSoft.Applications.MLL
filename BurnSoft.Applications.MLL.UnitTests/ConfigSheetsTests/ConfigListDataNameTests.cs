de80f0cb85ad35cb53fc5f432a4b4fb9c0f3677b
BurnSoft <bs.github@burnsoft.net>
2026-03-25 11:32:46 -0400

    #19 added rename test

--- BurnSoft.Applications.MLL.UnitTests/ConfigSheetsTests/ConfigListDataNameTests.cs
+++ BurnSoft.Applications.MLL.UnitTests/ConfigSheetsTests/ConfigListDataNameTests.cs
@@ -200,6 +200,28 @@ namespace BurnSoft.Applications.MLL.UnitTests.ConfigSheetsTests
         }
 
         [TestMethod, TestCategory("Config Sheets - Config Name Data")]
+        public void RenameTest()
+        {
+            bool bAns = false;
+            try
+            {
+                AddTestConfigNameExists();
+                PrintTestConfigNames();
+                long id = ConfigListDataName.GetId(_databasePath, _ConfigName, out _);
+                bool value = ConfigListDataName.Rename(_databasePath, id, $"(COPY) - {_ConfigName}", out _errOut);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+                TestContext.WriteLine($"VALUE: {value}");
+                PrintTestConfigNames("AFTER");
+                bAns = true;
+            }
+            catch (Exception ex)
+            {
+                TestContext.WriteLine(ex.Message);
+            }
+            General.HasTrueValue(bAns, _errOut);
+        }
+
+        [TestMethod, TestCategory("Config Sheets - Config Name Data")]
         public void DeleteTest()
         {
             bool bAns = false;
