44602525652e27ccdc8468d2efc66aa68b09cf17
BurnSoft <bs.github@burnsoft.net>
2026-03-27 13:40:13 -0400

    #36  added hull update qty for hulls

--- BurnSoft.Applications.MLL/Inventory/ShotgunHullInventory.cs
+++ BurnSoft.Applications.MLL/Inventory/ShotgunHullInventory.cs
@@ -409,6 +409,30 @@ namespace BurnSoft.Applications.MLL.Inventory
             return bAns;
         }
         /// <summary>
+        /// Updates the qty.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="newQty">The new qty.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool UpdateQty(string databasePath, long id, int newQty, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                string sql = $"UPDATE List_SG_Case set Qty={newQty} where id={id}";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("UpdateQty", e);
+            }
+            return bAns;
+        }
+
+        /// <summary>
         /// Deletes the specified database path.
         /// </summary>
         /// <param name="databasePath">The database path.</param>
