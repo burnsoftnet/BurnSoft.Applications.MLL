38ff695fe9453503c93aaedd40a8af290c1214f2
BurnSoft <bs.github@burnsoft.net>
2026-04-02 10:38:24 -0400

    #19 added Unit test for calculations

---
+++ BurnSoft.Applications.MLL.UnitTests/Helpers/GeneralCalculationsTests.cs
@@ -0,0 +1,40 @@
+﻿using BurnSoft.Applications.MLL.Helpers;
+using BurnSoft.Applications.MLL.UnitTests.Settings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.Applications.MLL.UnitTests.Helpers
+{
+    [TestClass]
+    public class GeneralCalculationsTests
+    {
+        /// <summary>
+        /// Gets or sets the test context.
+        /// </summary>
+        /// <value>The test context.</value>
+        public TestContext TestContext { get; set; }
+        /// <summary>
+        /// The error out
+        /// </summary>
+        private string _errOut;
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            // Vs2019.GetSetting("", TestContext);
+            _errOut = @"";
+        }
+
+        [TestMethod, TestCategory("Helpers - Calculations")]
+        public void CalculateMetallicRoundsToMakeTest()
+        {
+            long ConverThisToNumberExpected = 1000;
+            long roundsCanMake = GeneralCalculations.CalculateMetallicRoundsToMake(bulletQty: 1000, caseQty: 5000, primerQty: 8000,
+                powderQty: 6659.43, powderMidRangeLoad: 5.6, out _errOut);
+            TestContext.WriteLine($"RETURNED VALUE: {roundsCanMake}, expected: {ConverThisToNumberExpected}");
+            General.HasTrueValue(roundsCanMake == ConverThisToNumberExpected, _errOut);
+        }
+    }
+}
