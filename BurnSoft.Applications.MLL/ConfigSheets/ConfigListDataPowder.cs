2190fa0498ca0b57ebfa3b1c8ea17e5440684d47
BurnSoft <bs.github@burnsoft.net>
2026-04-02 13:11:04 -0400

    #19 fixed override

--- BurnSoft.Applications.MLL/ConfigSheets/ConfigListDataPowder.cs
+++ BurnSoft.Applications.MLL/ConfigSheets/ConfigListDataPowder.cs
@@ -238,7 +238,7 @@ namespace BurnSoft.Applications.MLL.ConfigSheets
                 if (errOut.Length > 0) throw new Exception(errOut);
                 foreach (ConfigListPowderData l in lst)
                 {
-                    lAns = l.Id;
+                    lAns = l.PowderId;
                     powderLoad = l.LoadMid;
                 }
             }
