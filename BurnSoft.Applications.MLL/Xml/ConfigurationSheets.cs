7c0191f1d2527e87ad345daa244326d61f75ba44
BurnSoft <bs.github@burnsoft.net>
2026-04-02 15:54:17 -0400

    #46 Started XML Classes for metallic config sheets

---
+++ BurnSoft.Applications.MLL/Xml/ConfigurationSheets.cs
@@ -0,0 +1,98 @@
+﻿using BurnSoft.Universal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Applications.MLL.Xml
+{
+    /// <summary>
+    /// Class ConfigurationSheets XML Export and save to file for Metallic Configs.
+    /// </summary>
+    public class ConfigurationSheets
+    {
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string _classLocation = "BurnSoft.Applications.MLL.Xml.ConfigurationSheets";
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        #endregion     
+        public static bool Generate(string databasePath, long configId, string filePath, out string errOut)
+        {
+            bool bAns = false;
+            errOut = "";
+            try
+            {
+                string body = $"<?xml version=\"1.0\" encoding=\"utf-8\" ?>{Environment.NewLine}";
+                body += $"<Inventory>{Environment.NewLine}";
+                //TODO Add Function Here
+                body += $"</Inventory>{Environment.NewLine}";
+                body = body.Replace("&", XmlConstants.Ampersand);
+                FileIO obj = new FileIO();
+                obj.DeleteFile(filePath);
+                obj.AppendToFile(filePath, body);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Generate", e);
+            }
+            return bAns;
+        }
+
+        private static string LineFormat(string xmlField, string value)
+        {
+            return $"<{xmlField}>{value}</{xmlField}>";
+        }
+
+        private static string GenerateConfigSection(string databasePath, long configId, out string errOut)
+        {
+            errOut = "";
+            string body = "";
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return body ;
+        }
+    }
+}
