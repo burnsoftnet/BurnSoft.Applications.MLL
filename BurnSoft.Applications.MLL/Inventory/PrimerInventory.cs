eee8bc0df09b30d173ed6b0ab45a24e7ea4adeaa
BurnSoft <bs.github@burnsoft.net>
2026-04-07 12:11:34 -0400

    #19  Added get name for primer

--- BurnSoft.Applications.MLL/Inventory/PrimerInventory.cs
+++ BurnSoft.Applications.MLL/Inventory/PrimerInventory.cs
@@ -171,6 +171,36 @@ namespace BurnSoft.Applications.MLL.Inventory
             return lAns;
         }
         /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.String.</returns>
+        /// <exception cref="System.Exception"></exception>
+        public static string GetName(string databasePath, long id, out string errOut)
+        {
+            errOut = "";
+            string sAns = "";
+            try
+            {
+                string sql = $"Select * from General_Primer where id={id}";
+                List<PrimerListings> lst = GetList(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                foreach (PrimerListings i in lst)
+                {
+                    sAns = i.Name;
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetName", e);
+            }
+            return sAns;
+        }
+
+        /// <summary>
         /// Gets the type of the primer.
         /// </summary>
         /// <param name="databasePath">The database path.</param>
