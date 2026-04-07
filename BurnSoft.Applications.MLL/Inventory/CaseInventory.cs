eefd759a5f45f6aadcb3577985f169a97f248f17
BurnSoft <bs.github@burnsoft.net>
2026-04-07 15:30:48 -0400

    #19 added get name to case inventory

--- BurnSoft.Applications.MLL/Inventory/CaseInventory.cs
+++ BurnSoft.Applications.MLL/Inventory/CaseInventory.cs
@@ -169,6 +169,35 @@ namespace BurnSoft.Applications.MLL.Inventory
             return lAns;
         }
         /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <param name="databasePath">The database path.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="errOut">The error out.</param>
+        /// <returns>System.String.</returns>
+        /// <exception cref="System.Exception"></exception>
+        public static string GetName(string databasePath, long id, out string errOut)
+        {
+            errOut = "";
+            string sAns = "";
+            try
+            {
+                string sql = $"Select * from List_Case where id={id}";
+                List<CaseListings> lst = GetList(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                foreach (CaseListings i in lst)
+                {
+                    sAns = i.Name;
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetName", e);
+            }
+            return sAns;
+        }
+        /// <summary>
         /// Gets the details.
         /// </summary>
         /// <param name="databasePath">The database path.</param>
