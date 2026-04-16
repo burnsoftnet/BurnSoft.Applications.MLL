76cab226b464d6526243c51302abb595376e1a96
BurnSoft <bs.github@burnsoft.net>
2026-04-16 08:28:17 -0400

    #22 Started class to handle data for sg gauge table

---
+++ BurnSoft.Applications.MLL/Inventory/ShotgunGauges.cs
@@ -0,0 +1,274 @@
+﻿using BurnSoft.Applications.MLL.Types;
+using BurnSoft.Universal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace BurnSoft.Applications.MLL.Inventory
+{
+    /// <summary>
+    /// Class ShotgunGauges handles the data in the List_SG_Gauge table
+    /// </summary>
+    public class ShotgunGauges
+    {
+        #region "Exception Error Handling"
+
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string ClassLocation = "BurnSoft.Applications.MLL.Inventory.ShotgunGauges";
+
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) =>
+            $"{ClassLocation}.{functionName} - {e.Message}";
+
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) =>
+            $"{ClassLocation}.{functionName} - {e.Message}";
+
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) =>
+            $"{ClassLocation}.{functionName} - {e.Message}";
+
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) =>
+            $"{ClassLocation}.{functionName} - {e.Message}";
+
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) =>
+            $"{ClassLocation}.{functionName} - {e.Message}";
+
+        #endregion                        
+
+        private static List<ShotgunGaugeData> GetData(DataTable dt, out string errOut)
+        {
+            List<ShotgunGaugeData> lst = new List<ShotgunGaugeData>();
+            errOut = "";
+            try
+            {
+                foreach (DataRow d in dt.Rows)
+                {
+                    lst.Add(new ShotgunGaugeData()
+                    {
+                        Id = Convert.ToInt32(d["id"]),
+                        Name = d["ga"] != DBNull.Value ? d["ga"].ToString().Trim() : "",
+                        LastSync = d["sync_lastupdate"] != DBNull.Value ? d["sync_lastupdate"].ToString().Trim() : DateTime.Now.ToString()
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetData", e);
+            }
+            return lst;
+        }
+
+        private static List<ShotgunGaugeData> GetList(string databasePath, string sql, out string errOut)
+        {
+            List<ShotgunGaugeData> lst = new List<ShotgunGaugeData>();
+            errOut = "";
+            try
+            {
+                DataTable dt = Database.GetDataFromTable(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                lst = GetData(dt, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetList", e);
+                errOut = $"{errOut}{Environment.NewLine}SQL: {sql}";
+            }
+            return lst;
+        }
+
+        public static List<ShotgunGaugeData> GetAll(string databasePath, out string errOut)
+        {
+            string sql = $"Select * from List_SG_Gauge order by FType ASC";
+            return GetList(databasePath, sql, out errOut);
+        }
+
+        public static long GetId(string databasePath, string name, out string errOut)
+        {
+            errOut = "";
+            long lAns = 0;
+            try
+            {
+                string sql = $"Select * from List_SG_Gauge where FType='{name}'";
+                List<ShotgunGaugeData> lst = GetList(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                foreach (ShotgunGaugeData i in lst)
+                {
+                    lAns = i.Id;
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetId", e);
+            }
+            return lAns;
+        }
+
+        public static List<ShotgunGaugeData> GetDetails(string databasePath, string name, out string errOut)
+        {
+            string sql = $"Select * from List_SG_Gauge where FType='{name}'";
+            return GetList(databasePath, sql, out errOut);
+        }
+
+        public static List<ShotgunGaugeData> GetDetails(string databasePath, long id, out string errOut)
+        {
+            string sql = $"Select * from List_SG_Gauge where id={id}";
+            return GetList(databasePath, sql, out errOut);
+        }
+
+        public static string GetGaugeName(string databasePath, long id, out string errOut)
+        {
+            string sAns = "";
+            errOut = "";
+            try
+            {
+                string sql = $"Select * from List_SG_Gauge where id={id}";
+                List<ShotgunGaugeData> lst = GetList(databasePath, sql, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                foreach (ShotgunGaugeData i in lst)
+                {
+                    sAns = i.Name;
+                }
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("GetGaugeName", e);
+            }
+            return sAns;
+        }
+
+        public static bool DataExists(string databasePath, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                List<ShotgunGaugeData> lst = GetAll(databasePath, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                bAns = lst.Count > 0;
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("DataExists", e);
+            }
+            return bAns;
+        }
+
+        public static bool DataExists(string databasePath, string name, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+
+                List<ShotgunGaugeData> lst = GetDetails(databasePath, name, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                bAns = lst.Count > 0;
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("DataExists", e);
+            }
+            return bAns;
+        }
+
+        public static bool Add(string databasePath, string name, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                string sql = $"INSERT INTO List_SG_Gauge(ga, sync_lastupdate) VALUES('{name}', Now())";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Add", e);
+            }
+            return bAns;
+        }
+
+        public static bool Update(string databasePath, long id, string name, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                BSOtherObjects o = new BSOtherObjects();
+                string sql = $"UPDATE List_SG_Gauge set ga='{name}', sync_lastupdate=Now() where id={id}";
+
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Update", e);
+            }
+            return bAns;
+        }
+
+        public static bool Delete(string databasePath, long id, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                string sql = $"DELETE from List_SG_Gauge where id={id}";
+                bAns = Database.Execute(databasePath, sql, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+            return bAns;
+        }
+
+        public static bool Delete(string databasePath, string name, out string errOut)
+        {
+            errOut = "";
+            bool bAns = false;
+            try
+            {
+                long id = GetId(databasePath, name, out errOut);
+                if (errOut.Length > 0) throw new Exception(errOut);
+                bAns = Delete(databasePath, id, out errOut);
+            }
+            catch (Exception e)
+            {
+                errOut = ErrorMessage("Delete", e);
+            }
+            return bAns;
+        }
+    }
+}
