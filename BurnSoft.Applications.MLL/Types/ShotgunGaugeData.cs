3046f6b0a84a5658d338790b2692a496dc936440
BurnSoft <bs.github@burnsoft.net>
2026-04-16 08:15:56 -0400

    #22 added list container for the sg gauge table

---
+++ BurnSoft.Applications.MLL/Types/ShotgunGaugeData.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace BurnSoft.Applications.MLL.Types
+{
+    /// <summary>
+    /// Class ShotgunGaugeData is the list container for the List_SG_Gauge Table
+    /// </summary>
+    public class ShotgunGaugeData
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the name which is the gc column
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the last synchronize for the sync_lastupdate column
+        /// </summary>
+        /// <value>The last synchronize.</value>
+        public string LastSync { get; set; }
+    }
+}
